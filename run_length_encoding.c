f97e82b9e9e6234adeee2ca2f90b62e00b123ae2
Joachim Lerman <joachimlerman@gmail.com>
2016-10-23 23:47:04 -0700

    wanted to do my own little compression algorithm thing

---
+++ run_length_encoding.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char ** argv)
+{
+    FILE *file = fopen("file_to_encode.txt", "r");
+    FILE *output = fopen("encoded_file.txt", "w");
+    char *code;
+    size_t n = 0;
+    int c;
+    int count = 0;
+
+    if (file == NULL)
+        return -1; //return NULL; //could not open file
+
+    code = malloc(1000);
+
+    while ((c = fgetc(file)) != EOF)
+    {
+        code[n++] = (char) c;
+        //printf("%c \n", (char) c);
+        if(c == code[n-2])
+        {
+            count++;
+        }
+        else
+        {
+            if(count < 5) //if it is 4 characters/bytes it costs more bytes to represent with an integer ex: 1B is larger than just B
+            {
+                for(int i = 0; i <= count; i++)
+                {
+                    fprintf(output, "%c", (char) code[n-2]);
+                }
+            }
+            else 
+            {
+                fprintf(output, "%d%c", count + 1, (char) code[n-2]);
+            }
+            count = 0;
+        }
+    }
+    fclose(file);
+    fclose(output);
+
+    //printf("%s", code);
+    // don't forget to terminate with the null character
+    code[n] = '\0';        
+
+    return 0;
+
+}
